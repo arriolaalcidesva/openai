11342489a7a5986a2ad78132bf56d8141619d510
arriolaalcidesva <arriolaalcides@gmail.com>
2023-05-04 20:48:35 -0300

    Prueba

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-// middlewares
+// middlewares aux
 app.use(compression());
 app.use(cors());
 app.use(express.json());
